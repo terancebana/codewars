48a4490c24003e77c827b05ef326a3c5c97b6e86
BANA Terance <t.cyuzuzo@alustudent.com>
2025-05-23 07:56:28 +0200

    Solved

--- Javascript/6kyu/kids-and-candies/script.js
+++ Javascript/6kyu/kids-and-candies/script.js
@@ -1,6 +1,4 @@
-function lcm(a, b) {
-    return Math.abs(a * b) / gcd(a, b);
-  }
+function candiesToBuy( kids ){
   
   function gcd(a, b) {
     while (b) {
@@ -10,10 +8,14 @@ function lcm(a, b) {
     }
     return a;
   }
-  
-  let result = 1;
-  for (let i = 1; i <= maxKids; i++) {
-    result = lcm(result, i);
-  }
-  
-  return result;
+  function lcm(a, b) {
+      return (a * b) / gcd(a, b);
+    }
+
+    let minCandies = 1;
+    for (let i = 1; i <= kids; i++) {
+      minCandies = lcm(minCandies, i);
+    }
+
+    return minCandies;
+}
