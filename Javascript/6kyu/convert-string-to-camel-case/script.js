99e99b38f00894b4cbd84bb8f08662ca4dbadbc1
BANA Terance <cyzterance06@gmail.com>
2025-08-02 14:56:28 +0200

    Solved

---
+++ Javascript/6kyu/convert-string-to-camel-case/script.js
@@ -0,0 +1,15 @@
+function toCamelCase(str){
+  if(str){
+    str = str.split('')
+    let result = []
+    for(let i = 0; i < str.length; i++){
+        if(str[i] === '-' || str[i] === '_'){
+            str[i+1] = str[i+1].toUpperCase()
+            result = str.splice(i, 1)
+        }
+    }
+    return str.join('')
+  }else{
+      return "An empty string was provided but not returned"
+  }
+}
