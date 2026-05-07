14f16f8698feeea10f8f8d1dca90081254f330c5
terancebana <cyzterance06@gmail.com>
2026-05-07 09:12:10 +0200

    solved 3 kata - 2026-05-07

---
+++ solutions/2026-05-07/7kyu_python_string_ends_with.py
@@ -0,0 +1,8 @@
+# Kata: String ends with?
+# Rank: 7 kyu
+# Solved: 2026-05-07
+# Source: https://www.codewars.com/kata/string-ends-with
+# -----------------------------------------------
+
+def solution(text, ending):
+    return text.endswith(ending)
